9e34fdadf879126c9cbf879c8fc2528b208a294f
m3ndax <adrian.goessl@outlook.com>
2022-09-17 17:49:23 +0200

    update code and comment

---
+++ Electrical Engineering/OHM.cs
@@ -0,0 +1,27 @@
+//use the system
+using System;
+
+//namespace for OHM
+namespace OHM
+{
+    //class for the OHM calculator
+    class OHM_program
+    {
+        //define the datatypes for the I and R
+        static double current;
+        static double resistance;
+
+        //main function
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an application to find the voltage\n------------------------------------------");
+            Console.WriteLine("Enter the I value");
+            current = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the R value");
+            resistance = double.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------");
+            double voltage = current * resistance;
+            Console.WriteLine("{1} Ampere * {2} Ohm = {0} Volt", voltage, current, resistance);
+        }
+    }
+}
