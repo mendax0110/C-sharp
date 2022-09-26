703ce6ea2742b38d03641673b7bd1f0a86111d69
m3ndax <adrian.goessl@outlook.com>
2022-09-26 20:09:41 +0200

    update code and comment

---
+++ math/calc.cs
@@ -0,0 +1,48 @@
+// use the system 
+using System;
+
+// declare variable and then initialize to zero
+int num1 = 0;
+int num2 = 0;
+
+    //display title as the C# console calculator app
+    Console.WriteLine("Console Calculator in C\r");
+    Console.WriteLine("------------------------\n");
+
+    // ask the user to type the first number
+    Console.WriteLine("Type a number, and then press Enter");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    // ask the user to type the second number
+    Console.WriteLine("Type another number, and then press Enter");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    // ask the user to choose an option
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("\ta - Add");
+    Console.WriteLine("\ts - Subtract");
+    Console.WriteLine("\tm - Multiply");
+    Console.WriteLine("\td - Divide");
+    Console.Write("Your option? ");
+
+    // use a switch statement to do the math
+    switch(Console.ReadLine())
+    {
+        case "a":
+            Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+            break;
+        case "s":
+            Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+            break;
+        case "m":
+            Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+            break;
+        case "d":
+            Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+            break;
+    }
+
+    // wait for the user to respond before closing
+    Console.Write("Press any key to close the Calculator console app...");
+    Console.ReadKey();
+    
