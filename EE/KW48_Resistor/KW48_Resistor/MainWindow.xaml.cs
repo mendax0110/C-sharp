614dfa53a4495eb49c44caab035a6ff8ac401124
m3ndax <adrian.goessl@outlook.com>
2022-12-06 20:11:34 +0100

    update new project for resistance calcuation

---
+++ EE/KW48_Resistor/KW48_Resistor/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace ResistanceCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Parse the input values
+            double voltage = double.Parse(voltageTextBox.Text);
+            double current = double.Parse(currentTextBox.Text);
+
+            // Calculate the resistance
+            double resistance = voltage / current;
+
+            // Display the result
+            resultLabel.Content = $"The resistance is: {resistance:F2} ohms";
+        }
+    }
+}
