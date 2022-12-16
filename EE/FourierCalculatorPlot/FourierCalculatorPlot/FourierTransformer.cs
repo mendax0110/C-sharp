9f4175b9552e5f61975effc53ba311494b79e2d3
m3ndax <adrian.goessl@outlook.com>
2022-12-16 22:08:01 +0100

    a simple program to calculate and plot fourierwaves into a graph

---
+++ EE/FourierCalculatorPlot/FourierCalculatorPlot/FourierTransformer.cs
@@ -0,0 +1,79 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Numerics;
+
+namespace FourierCalculatorPlot
+{
+    internal class FourierTransformer
+    {
+        public PlotModel? Model { get; internal set; }
+
+        internal void Plot(double[] data)
+        {
+            // Calculate the number of samples
+            int n = data.Length;
+
+            // Check if the number of samples is a power of 2
+            if ((n & (n - 1)) != 0)
+            {
+                throw new ArgumentException("The number of samples must be a power of 2.");
+            }
+
+            // Initialize the complex array for the FFT
+            Complex[] fftData = new Complex[n];
+            for (int i = 0; i < n; i++)
+            {
+                fftData[i] = new Complex(data[i], 0);
+            }
+
+            // Perform the FFT
+            FFT(fftData, n, 1);
+
+            // Create the PlotModel
+            Model = new PlotModel();
+            Model.Title = "Fourier Transform";
+
+            // Create the X axis
+            LinearAxis xAxis = new LinearAxis();
+            xAxis.Position = AxisPosition.Bottom;
+            xAxis.Title = "Frequency (Hz)";
+            Model.Axes.Add(xAxis);
+
+            // Create the Y axis
+            LinearAxis yAxis = new LinearAxis();
+            yAxis.Title = "Amplitude";
+            Model.Axes.Add(yAxis);
+
+            // Create the line series for the FFT
+            LineSeries lineSeries = new LineSeries();
+            lineSeries.Title = "FFT";
+            lineSeries.Color = OxyColor.FromRgb(255, 0, 0);
+            for (int i = 0; i < n / 2; i++)
+            {
+                lineSeries.Points.Add(new DataPoint(i / (double)n, fftData[i].Magnitude));
+            }
+            Model.Series.Add(lineSeries);
+        }
+
+        private static void FFT(Complex[] data, int n, int sign)
+        {
+            // Base case
+            if (n == 1)
+            {
+                return;
+            }
+
+            // Split the array into even and odd samples
+            Complex[] even = new Complex[n / 2];
+            Complex[] odd = new Complex[n / 2];
+            for (int i = 0; i < n / 2; i++)
+            {
+                even[i] = data[2 * i];
+                odd[i] = data[2 * i + 1];
+            }
+        }
+
+    }
+}
---
+++ EE/FourierCalculatorPlot/FourierCalculatorPlot/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Numerics;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace FourierCalculatorPlot
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // get user input
+            double amplitude = Convert.ToDouble(amplitudeInput.Text);
+            double frequency = Convert.ToDouble(frequencyInput.Text);
+            double phase = Convert.ToDouble(phaseInput.Text);
+            double time = Convert.ToDouble(timeInput.Text);
+
+            // calculate
+            double result = amplitude * Math.Sin(2 * Math.PI * frequency * time + phase);
+
+            // display result
+            resultLabel.Content = result.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // get user input
+            double amplitude = Convert.ToDouble(amplitudeInput.Text);
+            double frequency = Convert.ToDouble(frequencyInput.Text);
+            double phase = Convert.ToDouble(phaseInput.Text);
+            double time = Convert.ToDouble(timeInput.Text);
+
+            // calculate waveform data
+            int numSamples = 1024;
+            double[] data = new double[numSamples];
+            for (int i = 0; i < numSamples; i++)
+            {
+                double t = i / (double)numSamples;
+                data[i] = amplitude * Math.Sin(2 * Math.PI * frequency * t + phase);
+            }
+
+            // calculate and plot Fourier transform
+            FourierCalculatorPlot.FourierTransformer fourierTransformer = new FourierCalculatorPlot.FourierTransformer();
+            fourierTransformer.Plot(data);
+            plotView.Model = fourierTransformer.Model;
+        }
+
+        private void timeInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
