c40248d80262a6882991dd201133de206e4ae3d6
m3ndax <adrian.goessl@outlook.com>
2022-12-20 23:07:03 +0100

    simple simulator calculate and plot into graph, graph is not working atm. fixing this soon

---
+++ EE/FusionSimulator/FusionSimulator/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using FusionSimulator.Simulations;
+
+namespace FusionSimulator
+{
+    public partial class MainWindow : Window
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection();
+        }
+
+        private void SimulateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the user input values
+            string fuelType = fuelComboBox.Text;
+            double fuelAmount = 0;
+            double temperature = 0;
+            double pressure = 0;
+            try
+            {
+                fuelAmount = double.Parse(fuelAmountTextBox.Text);
+                temperature = double.Parse(temperatureTextBox.Text);
+                pressure = double.Parse(pressureTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid input values");
+                return;
+            }
+
+            // Create a FusionSimulation object and call its Simulate method
+            FusionSimulation simulation = new FusionSimulation(fuelType, fuelAmount, temperature, pressure);
+            List<double> results = simulation.Simulate();
+            if (results == null || results.Count == 0)
+            {
+                MessageBox.Show("Simulation did not return any results");
+                return;
+            }
+
+            // Update the SeriesCollection with the simulation results
+            SeriesCollection.Clear();
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Energy Output (J)",
+                Values = new ChartValues<double>(results)
+            });
+        }
+    }
+}
---
+++ EE/FusionSimulator/FusionSimulator/Simulations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FusionSimulator.Simulations
+{
+    public class FusionSimulation
+    {
+        private readonly string _fuelType;
+        private readonly double _fuelAmount;
+        private readonly double _temperature;
+        private readonly double _pressure;
+
+        public FusionSimulation(string fuelType, double fuelAmount, double temperature, double pressure)
+        {
+            _fuelType = fuelType;
+            _fuelAmount = fuelAmount;
+            _temperature = temperature;
+            _pressure = pressure;
+        }
+
+        public List<double> Simulate()
+        {
+            // Perform the fusion simulation and return the results
+            List<double> results = new List<double>();
+
+            // Here you can add the code to perform the fusion simulation using the input parameters (_fuelType, _fuelAmount, _temperature, _pressure)
+            // The simulation could involve calculations, data processing, and/or interacting with other systems or resources
+            // For the purposes of this example, we will assume that the fusion simulation involves calculating the energy produced by the fusion reaction
+
+            double energy;
+            if (_fuelType == "deuterium-tritium")
+            {
+                // Use the deuterium-tritium fusion reaction to calculate the energy produced
+                energy = _fuelAmount * 17.6 * Math.Pow(10, 6);  // energy in joules
+            }
+            else if (_fuelType == "deuterium-deuterium")
+            {
+                // Use the deuterium-deuterium fusion reaction to calculate the energy produced
+                energy = _fuelAmount * 4.0 * Math.Pow(10, 6);  // energy in joules
+            }
+            else
+            {
+                // Other fuel types are not supported, so return 0 energy
+                energy = 0;
+            }
+
+            results.Add(energy);
+
+            return results;
+        }
+    }
+}
