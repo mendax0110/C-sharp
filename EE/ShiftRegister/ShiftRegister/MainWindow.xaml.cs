149ecc52c9326ffdc0b4fd7902aab57c14a6ccb6
m3ndax <adrian.goessl@outlook.com>
2023-01-04 11:01:48 +0100

    shift register simulator with gui

---
+++ EE/ShiftRegister/ShiftRegister/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ShiftRegister;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftRegister
+{
+    public partial class MainWindow : Window
+    {
+        private SN74HC595N _shiftRegister;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _shiftRegister = new SN74HC595N(8);
+            UpdateRegisterDisplay();
+        }
+
+        private void ShiftLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool inputBit = GetInputBit();
+            _shiftRegister.ShiftLeft(inputBit);
+            UpdateRegisterDisplay();
+        }
+
+        private void ShiftRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool inputBit = GetInputBit();
+            _shiftRegister.ShiftRight(inputBit);
+            UpdateRegisterDisplay();
+        }
+
+        private bool GetInputBit()
+        {
+            return inputTextBox.Text == "1";
+        }
+
+        private void UpdateRegisterDisplay()
+        {
+            registerListBox.Items.Clear();
+            foreach (bool bit in _shiftRegister.GetRegister())
+            {
+                registerListBox.Items.Add(bit ? "1" : "0");
+            }
+        }
+    }
+}
---
+++ EE/ShiftRegister/ShiftRegister/ShiftRegister.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftRegister
+{
+    public class SN74HC595N
+    {
+        private bool[] _register;
+        private int _numBits;
+
+        public SN74HC595N(int numBits)
+        {
+            _numBits = numBits;
+            _register = new bool[numBits];
+        }
+
+        public void ShiftLeft(bool inputBit)
+        {
+            for (int i = _numBits - 1; i > 0; i--)
+            {
+                _register[i] = _register[i - 1];
+            }
+            _register[0] = inputBit;
+        }
+
+        public void ShiftRight(bool inputBit)
+        {
+            for (int i = 0; i < _numBits - 1; i++)
+            {
+                _register[i] = _register[i + 1];
+            }
+            _register[_numBits - 1] = inputBit;
+        }
+
+        public bool[] GetRegister()
+        {
+            return _register;
+        }
+    }
+}
