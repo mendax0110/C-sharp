81d3fccc6dde161230c67a34a4ad4ac1fc12a72f
m3ndax <adrian.goessl@outlook.com>
2023-01-21 15:20:51 +0100

    update comments, removed unused code

--- EE/ADC_Reader/ADC_Reader/MainWindow.xaml.cs
+++ EE/ADC_Reader/ADC_Reader/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO.Ports;
 
 namespace ADC_Reader
@@ -24,9 +13,9 @@ namespace ADC_Reader
         SerialPort serialPort;
         string receivedData;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public MainWindow()
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             InitializeComponent();
             serialPort = new SerialPort();
